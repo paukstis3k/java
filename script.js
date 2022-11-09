2ad21de7b0255c52b943c6e0e2465d2d4326c55b
Master <Paukstis3k@gmail.com>
2022-11-09 08:40:01 -0800

    commitas

---
+++ script.js
@@ -0,0 +1,18 @@
+// let birthdate - 1995;
+// let thisyear - 2022;
+
+
+
+// let sakinys = thisyear - birthdate
+
+// alert{yourage};
+
+
+// alertas su vardu prideta prie pavardes
+
+let firstname = "Artur";
+let lastname = "Andrejev";
+
+let name = `${firstname} ${lastname}`;
+
+alert(name);
