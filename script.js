62dfe69f52c63f2bf634318870145c63adc7e16c
Master <Paukstis3k@gmail.com>
2022-11-10 08:40:02 -0800

    Mokinames java

--- script.js
+++ script.js
@@ -10,9 +10,34 @@
 
 // alertas su vardu prideta prie pavardes
 
-let firstname = "Artur";
-let lastname = "Andrejev";
+// let firstname = "Artur";
+// let lastname = "Andrejev";
 
-let name = `${firstname} ${lastname}`;
+// let name = `${firstname} ${lastname}`;
 
-alert(name);
+// alert(name);
+
+// let x = 0.01
+// let tipas = typeof x;
+
+// console.log(tipas);
+
+// let svoris = prompt ("Koks jusu svoris?");
+// let svorisKg = Number(svoris);
+
+
+// console.log(svorisKg, typeof svorisKg);
+
+
+
+// let svoris = prompt("Koks jusu svoris? (kg)");
+// svoris = Number(svoris);
+
+// let ugis = Number(prompt("Koks jusu ugis? (cm)"));
+
+// const daugiklis = 10_000;
+
+// const BMI = (svoris / ugis ** 2) * daugiklis;
+
+// alert(`Jusu BMI: ${BMI}`);
+
