6f6c3bdaf0f6fa43437878960bf10f8a4c8cd867
Master <Paukstis3k@gmail.com>
2022-11-16 10:00:13 -0800

    alertai

--- script.js
+++ script.js
@@ -41,3 +41,121 @@
 
 // alert(`Jusu BMI: ${BMI}`);
 
+
+// const currentHours = Number(prompt("kiek dabar valandu?"))
+// const currentMinutes = Number(prompt("kiek dabar minuciu?"))
+
+// const shouldReduceHour = currentMinutes > 0;
+
+// const additionalHour = Number(shouldReduceHour);
+
+// const isNotTooLate = Number(currentHour < 18);
+
+// const hourDifference = (18 - currentHours - additionalHour) * isNotTooLate;
+// const minuteDifference = (60 - currentMinutes) * additionalHour * isNotTooLate;
+
+// alert(`Liko: ${hourDifference} val. ${minuteDifference} min.`);
+
+
+
+// const metai = prompt("Kurie dabar metai?");
+
+// if (metai === "2022") {
+//     alert("Tu teisus!");
+//     let x = 0;
+//     console.log(x);
+// } else {
+//     alert("Tu neteisus!");
+//     x = 1;
+// }
+
+// console.log(x);
+
+// const colour = prompt("Kokia spalva?(red,blue,yellow");
+// cons model = prompt("Koks modelis?(rasykite metus)");
+// const make = prompt("kokia marke?(ferrari, golfas)");
+// const mileage = prompt("kokia rida?(kiek km nuvaziuota?");
+
+
+// if (colour === "red") {
+//     // yes
+//     if (Number(model) > 2010) {
+//         alert("Buy!");
+//     } else if (mileage < 50000) {
+//         alert ("Buy");
+//     }
+//     else {
+//         alert("Don't buy!");
+//     }
+// } else {
+//     //no
+//     if (colour == "yellow") {
+//         if (make === "ferrari") {
+//             alert("Buy!");
+//         }
+//         else {
+//             alert("Don't buy!");
+//         }
+//     } else {}
+// }
+
+
+// const svoris = Number(prompt("Koks Jusu svoris (kg?)"));
+// const ugis = Number(prompt("Koks Jusu ugis (m)?"))**2;
+
+// const KMI = svoris / ugisKv;
+
+// if (KMI < 10.5) {
+//     alert("Under wight");
+// } else if (KMI < 30) {
+//     alert("Normal");
+// } else if (KMI < 30) {
+//     alert("Overweight");
+// } else if (KMI < 35) {
+//     alert("Obesity (Class I)");
+// } else if
+
+
+
+// const gender = prompt("Kokia jusu lytis? ( vyras . mteris)");
+// const weight = Number(prompt("Koks jusu svoris? ( kg )"));
+// const height = Number(prompt("Koks jusu ugis? ( cm )"));
+// const age = Number(prompt("Koks jusu amzius? ( metais )"));
+// const activityLevel = Number(prompt("Koks jusu aktyvumo lygis? ( 0 , 1 , 2 , 3 , 4)"));
+
+// let BMR;
+
+// if (gender === "vyras") {
+//     BMR = (66.5 + (13.75 * weight) + (5.003 + height) - (6.75 * age))
+//     console.log("vyras")
+// } 
+// else {
+//     BMR = (655.1 + (9.563 * weight) + (1.850 + height) - (4.676 * age))
+//     console.log("moteris")
+
+// }
+// console.log(BMR);
+
+// if (activityLevel === 0) {
+//     alert(BMR * 1.2)
+// }
+// else if (activityLevel === 1) {
+//     alert(BMR = 1.375)
+// }
+// else if (activityLevel === 2) {
+//     alert(BMR = 1.55)
+// }
+// else if (activityLevel === 3) {
+//     alert(BMR = 1.725)
+// }
+// else if (activityLevel === 4) {
+//     alert(BMR = 1.9)
+// }
+
+
+
+
+
+
+// const answer = prompt("Kazkoks klausimas (y/n")
+
