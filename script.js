7783c777fc8a980f1752413d95e28252bf948ed9
Master <Paukstis3k@gmail.com>
2023-01-05 09:21:28 -0800

    modals

--- script.js
+++ script.js
@@ -1,96 +1,88 @@
-const attackBtn = document.querySelector("#attack-btn");
+const firstModal = document.querySelector(".first-modal");
+const lastNameInput = document.querySelector("#first-name");
+const emailNameInput = document.querySelector("#email-name");
+const continueBtn = document.querySelector("#continue-button");
+const backBtn = document.querySelector("#back-button");
 
-const defendBtn = document.querySelector("#defend-btn");
+const step0Content = document.querySelector(".step-0");
+const step1Content = document.querySelector(".step-1");
+const step2Content = document.querySelector(".step-2");
+const step3Content = document.querySelector(".step-3");
+const hiddenCLass = document.querySelector(".hidden");
+const primaryAddressInput = document.querySelector();
 
-const healBtn = document.querySelector("#heal-btn");
+const result = {
+    step: 0,
+    user: {},
+  };
 
-const resultDragonHealth = document.querySelector("#dragon-health");
+continueBtn.addEventListener("click", onContinue);
 
-const resultKnightHealth = document.querySelector("#knight-health");
-
-const footer = document.querySelector(".footer");
-
-const containerRight = document.querySelector(".container-right");
-
-const containerLeft = document.querySelector(".container-left");
-
-let currentRound = 1;
-
-attackBtn.addEventListener("click", knightAttack);
-
-defendBtn.addEventListener("click", knightDefend);
-
-healBtn.addEventListener("click", displayCombatLog);
-
-function getRandomNb(max) {
-  const randomInt = Math.ceil(Math.random() * max);
-  return randomInt;
-}
-
-resultDragonHealth.textContent = "200";
-function knightAttack() {
-  const attackDragonDamage = getRandomNb(10);
-  resultDragonHealth.textContent =
-    Number(resultDragonHealth.textContent) - attackDragonDamage;
-  dragonAttack(attackDragonDamage);
-}
-
-resultKnightHealth.textContent = "100";
-
-function dragonAttack(attackDragonDamage) {
-  const knightAttackDamage = getRandomNb(20);
-  resultKnightHealth.textContent =
-    Number(resultKnightHealth.textContent) - knightAttackDamage;
-  displayCombatLog(attackDragonDamage,knightAttackDamage);
-}
-
-function isknightDefend() {
-  let knightDefend = true;
-  dragonAttack();
-  if ((attackDragonDamage = 0)) {
-    displayCombatLog();
-  } else {
-    dragonAttack(attackDragonDamage);
+function onContinue(event) {
+  event.preventDefault();
+  if (result.step === 0) {
+    if (
+      !firstNameInput.value ||
+      !emailNameInput.value ||
+      !emailNameInput.validity.typeMismatch
+    ) {
+      if (!firstNameInput.value) {
+        applyErrorToField(firstNameInput);
+      }
+      if (!lastNameInput.value) {
+        applyErrorToField(lastNameInput);
+      }
+      if (!email.value) {
+        applyErrorToField(email);
+      }
+    } else {
+      result.user.firstName = firstNameInput.value;
+      result.user.lastName = lastNameInput.value;
+      result.user.email = emailInput.value;
+      result.step += 1;
+      displayWizardStep.WizardStep(1);
+    }
   }
 }
 
-function displayCombatLog(attackDragonDamage, knightAttackDamage) {
-  footer.style.display = "block";
-  const logContainer = document.createElement("div");
-
-  const logHeading = document.createElement("h2");
-  logHeading.textContent = "Round-" + currentRound;
-  currentRound++;
-  const knightInfo = document.createElement("p");
-  knightInfo.textContent = `Knight attacks and deals ${getRandomNb(
-    10
-  )} damage.`;
-
-  const dragonInfo = document.createElement("p");
-
-  dragonInfo.textContent = `Dragon attacks and deals ${getRandomNb(
-    20
-  )} damage.`;
-
-  footer.append(logContainer);
-
-  logContainer.append(logHeading, knightInfo, dragonInfo);
-
-  areTheyAlive(attackDragonDamage, knightAttackDamage);
-
-  console.log(attackDragonDamage, knightAttackDamage);
+function applyErrorToField(input) {
+  input.classList.add("error");
 }
 
-function areTheyAlive(attackDragonDamage, knightAttackDamage) {
-  if (
-    resultKnightHealth.textContent > 0 &&
-    resultDragonHealth.textContent <= 0
-  ) {
-    containerLeft.style.display = "none";
-  } else if (
-    resultKnightHealth.textContent < 0 &&
-    resultDragonHealth.textContent >= 0
-  ) {
-    containerRight.style.display = "none";
+function displayWizardStep(step) {
+  if (step === 0) {
+    step0Content.classList.add(hiddenCLass);
+    step1Content.classList.add(hiddenCLass);
+    step2Content.classList.add(hiddenCLass);
+    step3Content.classList.add(hiddenCLass);
+    backBtn.classListList.remove(hiddenCLass);
+  }
+  if (step === 1) {
+    step0Content.classList.add(hiddenCLass);
+    step1Content.classList.add(hiddenCLass);
+    step2Content.classList.add(hiddenCLass);
+    step3Content.classList.add(hiddenCLass);
+    backBtn.classListList.remove(hiddenCLass);
+  }
+  if (step === 2) {
+    step0Content.classList.add(hiddenCLass);
+    step1Content.classList.add(hiddenCLass);
+    step2Content.classList.add(hiddenCLass);
+    step3Content.classList.add(hiddenCLass);
+    backBtn.classListList.remove(hiddenCLass);
+  }
+  if (step === 3) {
+    step0Content.classList.add(hiddenCLass);
+    step1Content.classList.add(hiddenCLass);
+    step2Content.classList.add(hiddenCLass);
+    step3Content.classList.add(hiddenCLass);
+    backBtn.classListList.remove(hiddenCLass);
+  }
+  if (step === 4) {
+    step0Content.classList.add(hiddenCLass);
+    step1Content.classList.add(hiddenCLass);
+    step2Content.classList.add(hiddenCLass);
+    step3Content.classList.add(hiddenCLass);
+    backBtn.classListList.remove(hiddenCLass);
   }
 }
